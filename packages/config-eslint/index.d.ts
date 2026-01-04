3d6fe17329ecd107a3129c5c436d3bdc7b2cfa13
nct-sajid-mujawar <sajid.mujawar@ncircletech.com>
2026-01-05 05:10:47 +0530

    added type declaration fpr slint configurations

---
+++ packages/config-eslint/index.d.ts
@@ -0,0 +1,14 @@
+declare module '@repo/eslint-config/base' {
+  const config: any
+  export default config
+}
+
+declare module '@repo/eslint-config/react' {
+  const config: any
+  export default config
+}
+
+declare module '@repo/eslint-config/node' {
+  const config: any
+  export default config
+}
